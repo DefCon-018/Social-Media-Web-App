4a1ae46d8f130a831895197fb2436f5e2cc834fc
DefCon-018 <hp761570@gmil.com>
2020-06-17 18:30:15 +0530

    guest login done successfully
    Signed-off-by: DefCon-018 <hp761570@gmil.com>

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -1,3 +1,23 @@
+
 $(()=>{
-    $('#navbar').load('../component/navbar.html');
+    $('#navbar').load('../component/navbar.html', loginIfNeeded);
+    $('#footer').load('../component/footer.html')
 })
+
+function loginIfNeeded(){
+    let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null;
+    if(!currentUser){
+        $.post('/api/users', {}, (user)=>{
+            if(user){
+                console.log('registered current user as ', user.username)
+                window.localStorage.user = JSON.stringify(user);
+                currentUser = user;
+                $('#nav-username').text(currentUser.username);
+            }
+        })
+    }
+    else{
+        console.log('resumming session as ', currentUser.username);
+        $('#nav-username').text(currentUser.username)
+    }
+}
