944a78a9feec551461dc148a0a32a5d342d77806
DefCon-018 <hp761570@gmil.com>
2020-06-17 19:28:25 +0530

    post component work
    Signed-off-by: DefCon-018 <hp761570@gmil.com>

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -2,6 +2,7 @@
 $(()=>{
     $('#navbar').load('../component/navbar.html', loginIfNeeded);
     $('#footer').load('../component/footer.html')
+    $('#content').load('../component/all-article.html', loadPosts);
 })
 
 function loginIfNeeded(){
@@ -20,4 +21,24 @@ function loginIfNeeded(){
         console.log('resumming session as ', currentUser.username);
         $('#nav-username').text(currentUser.username)
     }
+}
+
+function loadPosts(){
+    $.get('/api/posts', (posts)=>{
+        for(p of posts){
+            $('#posts-container').append(`
+            <div class= "col-4">
+              <div class="card m-2">
+                <div class="card-body">
+                  <h5 class="card-title">${p.title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+                  <p class="card-text">${p.body}</p>
+                  <a href="#" class="card-link">Read</a>
+                  <a href="#" class="card-link">Like</a>
+                </div>
+              </div>
+            </div>
+            `)
+        }
+    })
 }
